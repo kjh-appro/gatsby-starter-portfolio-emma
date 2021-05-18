f1ebbb88d9446a4124f5bb138450c7d591f64ff4
kjh-appro <57822259+kjh-appro@users.noreply.github.com>
2021-05-18 15:25:04 +0900

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,18 +31,6 @@ module.exports = {
         background_color: `#fff`,
         theme_color: `#b83280`,
         display: `standalone`,
-        icons: [
-          {
-            src: `/android-chrome-192x192.png`,
-            sizes: `192x192`,
-            type: `image/png`,
-          },
-          {
-            src: `/android-chrome-512x512.png`,
-            sizes: `512x512`,
-            type: `image/png`,
-          },
-        ],
       },
     },
     `gatsby-plugin-offline`,
